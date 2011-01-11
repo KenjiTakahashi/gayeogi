289cd9258d60f413a9bdec9da55679d093bb6f35
kenjitakahashi <wozniakk@gmail.com>
2011-01-11 22:43:51 +0100

    started last.fm scrobbler implementation

---
+++ src/fetcher/plugins/last.fm.py
@@ -0,0 +1,31 @@
+# This is a part of Fetcher @ http://github.com/KenjiTakahashi/Fetcher/
+# Karol "Kenji Takahashi" Wozniak (C) 2010
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
+
+import pylast
+
+class Main(object):
+    name = u'Last.FM'
+    loaded = False
+    def __init__(self, parent, library, _, __):
+        pass
+    def load(self):
+        pass
+    def unload(self):
+        pass
+    def QConfiguration():
+        pass
+    QConfiguration = staticmethod(QConfiguration)
