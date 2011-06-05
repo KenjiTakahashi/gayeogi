c50fca0db3d87e069a076f71cf7025c1936fe500
kenjitakahashi <wozniakk@gmail.com>
2011-06-05 22:18:10 +0200

    polish translation

--- lang/main_pl_PL.ts
+++ lang/main_pl_PL.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="pl_PL" sourcelanguage="en_GB">
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL" sourcelanguage="en_GB">
 <context>
     <name>ConfirmationDialog</name>
     <message>
@@ -10,7 +11,7 @@
     <message>
         <location filename="fetcher/interfaces/confirmation.py" line="25"/>
         <source>Close &amp;without saving</source>
-        <translation>Ni&amp;e zapisuj</translation>
+        <translation>Zamknij b&amp;ez zapisu</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/confirmation.py" line="27"/>
@@ -28,42 +29,42 @@
     <message>
         <location filename="fetcher/main.py" line="180"/>
         <source>Module</source>
-        <translation type="unfinished"></translation>
+        <translation>Moduł</translation>
     </message>
     <message>
         <location filename="fetcher/main.py" line="180"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Typ</translation>
     </message>
     <message>
         <location filename="fetcher/main.py" line="180"/>
         <source>File/Entry</source>
-        <translation type="unfinished"></translation>
+        <translation>Plik/Wpis</translation>
     </message>
     <message>
         <location filename="fetcher/main.py" line="180"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Wiadomość</translation>
     </message>
     <message>
         <location filename="fetcher/main.py" line="313"/>
         <source>Database:Type:File/Entry:Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Baza:Typ:Plik/Wpis:Wiadomość</translation>
     </message>
     <message>
         <location filename="fetcher/main.py" line="322"/>
         <source>Logs saved</source>
-        <translation type="unfinished"></translation>
+        <translation>Logi zostały zapisane</translation>
     </message>
     <message>
         <location filename="fetcher/main.py" line="456"/>
         <source>Nothing to save...</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie ma co zapisać...</translation>
     </message>
     <message>
         <location filename="fetcher/main.py" line="458"/>
         <source>Saved</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisano</translation>
     </message>
 </context>
 <context>
@@ -71,127 +72,127 @@
     <message>
         <location filename="fetcher/interfaces/settings.py" line="46"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="46"/>
         <source>Threads</source>
-        <translation type="unfinished"></translation>
+        <translation>Wątki</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="77"/>
         <source>&amp;Up</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Góra</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="79"/>
         <source>&amp;Down</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Dół</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="81"/>
         <source>Behaviour</source>
-        <translation type="unfinished"></translation>
+        <translation>Zachowanie</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="82"/>
         <source>Search for all bands in all enabled databases.</source>
-        <translation type="unfinished"></translation>
+        <translation>Szukaj wszystkich wykonawców we wszytkich bazach.</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="82"/>
         <source>C&amp;rossed</source>
-        <translation type="unfinished"></translation>
+        <translation>K&amp;rzyżowy</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="87"/>
         <source>Search databases in order and in every next database, search only for bands not yet found elsewhere.</source>
-        <translation type="unfinished"></translation>
+        <translation>Przeszukuje bazy po kolei i w każdej kolejnej sprawdza tych wykonawców, których nie znalazł jeszcze w innej.</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="110"/>
         <source>Releases</source>
-        <translation type="unfinished"></translation>
+        <translation>Wydawnictwa</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="118"/>
         <source>&amp;Databases</source>
-        <translation type="unfinished"></translation>
+        <translation>Bazy &amp;danych</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="119"/>
         <source>Directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Katalog</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="122"/>
         <source>&amp;Browse</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Szukaj</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="128"/>
         <source>Ignores:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ignorowane:</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="139"/>
         <source>&amp;Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Dod&amp;aj</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="141"/>
         <source>&amp;Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Usuń</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="153"/>
         <source>&amp;Local</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Localna</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="163"/>
         <source>Lo&amp;gs</source>
-        <translation type="unfinished"></translation>
+        <translation>Lo&amp;gi</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="189"/>
         <source>&amp;Plugins</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Dodatki</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="190"/>
         <source>&amp;OK</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;OK</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="192"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished">&amp;Anuluj</translation>
+        <translation>&amp;Anuluj</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="218"/>
         <source>Directory field cannot be empty!</source>
-        <translation type="unfinished"></translation>
+        <translation>Pole wyboru katalogu nie może być puste!</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="302"/>
         <source>Here you can choose which databases should be searched, what releases to search for and how the search should behave.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutaj można wybrać, które bazy będą przeszukiwane, jakich typów wydawnictw szukać oraz w jaki sposób to robić.</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="304"/>
         <source>Here you can choose in which directory you files lies and which files to ignore while searching (you can use wilcards, like &apos;*&apos; or &apos;?&apos;)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutaj możesz wybrać w jakim folderze znajdują się Twoje pliki i jakie pliki ignorować przy przeszukiwaniu (dozwolone znaki wieloznaczności, jak &apos;*&apos; lub &apos;?&apos;)</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="306"/>
         <source>Here you can choose what kind of log messages should be displayed in the main window.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutaj możesz wybrać jakie rodzaje logów będą wyświetlane w głownym oknie.</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/settings.py" line="308"/>
         <source>Here you can choose and configure additional plugins.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutaj możesz wybrać i skonfigurować dodatki.</translation>
     </message>
 </context>
 <context>
@@ -199,92 +200,92 @@
     <message>
         <location filename="fetcher/interfaces/main.py" line="273"/>
         <source>Pattern: &lt;pair&gt;|&lt;pair&gt;, where &lt;pair&gt;=&lt;column_name&gt;:&lt;searching_phrase&gt;. Case insensitive, regexp allowed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wzorzec: &lt;para&gt;|&lt;para&gt;, gdzie &lt;para&gt;=&lt;nazwa_kolumny&gt;:&lt;szukane_wyrażenie&gt;. Wielkość liter nie ma znaczenia, dozwolone wyrażenia regularne.</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="276"/>
         <source>Clear logs table</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyczyść logi</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="277"/>
         <source>Cle&amp;ar</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Wyczyść</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="278"/>
         <source>Save logs to file</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisz logi do pliku</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="279"/>
         <source>Sa&amp;ve</source>
-        <translation type="unfinished"></translation>
+        <translation>Z&amp;apisz</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="280"/>
         <source>Artists</source>
-        <translation type="unfinished"></translation>
+        <translation>Wykonawcy</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="287"/>
         <source>0</source>
-        <translation type="unfinished"></translation>
+        <translation>0</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="284"/>
         <source>Albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Albumy</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="288"/>
         <source>Refresh local filesystem</source>
-        <translation type="unfinished"></translation>
+        <translation>Odśwież system plików</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="289"/>
         <source>&amp;Local</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Localny</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="290"/>
         <source>Change settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Zmień ustawienia</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="291"/>
         <source>S&amp;ettings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawi&amp;enia</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="292"/>
         <source>Refresh internet databases</source>
-        <translation type="unfinished"></translation>
+        <translation>Odśwież bazy danych</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="293"/>
         <source>&amp;Remote</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Bazy</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="294"/>
         <source>Save current database to disk.</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisz bazę na dysk.</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="295"/>
         <source>&amp;Save</source>
-        <translation type="unfinished">Zapi&amp;sz</translation>
+        <translation>Zapi&amp;sz</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="296"/>
         <source>Close the application</source>
-        <translation type="unfinished"></translation>
+        <translation>Zamknij aplikację</translation>
     </message>
     <message>
         <location filename="fetcher/interfaces/main.py" line="297"/>
         <source>&amp;Close</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Zamknij</translation>
     </message>
 </context>
 </TS>
